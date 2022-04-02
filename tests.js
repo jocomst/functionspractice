4c3966703a0166fb9f20e8bbf5e32a7a906cfae7
John Comstock <johncomstock@Johns-MacBook-Pro-2.local>
2022-04-01 21:44:50 -0400

    changes

--- tests.js
+++ tests.js
@@ -4,6 +4,48 @@
 // const results = document.querySelector(".results");
 // let resultsOfGame = document.createElement("h2");
 // throw new Error("oops");
+const p1Name = prompt("Player 1 name");
+const p2Name = prompt("Player 2 name");
+let player1Name;
+let player2Name;
+
+class gameVisuals {
+  choiceSelector = document.querySelectorAll(".choices");
+  player1Name = document.querySelector("player-one-name");
+  player2Name = document.querySelector("player-two-name");
+  constructor(game) {
+    this.game = game;
+    this.choiceSelector.forEach((el) => {
+      el.addEventListener("change", (e) => {
+        this.changeImage(e);
+        // console.log(e.target);
+      });
+    });
+    this.addPlayerNames();
+  }
+
+  changeImage(e) {
+    let player = e.target.previousSibling.previousSibling;
+    if (e.target.value === "rock") {
+      player.src =
+        "https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTc5NjIyODM0ODM2ODc0Mzc3/dwayne-the-rock-johnson-gettyimages-1061959920.jpg";
+    }
+    if (e.target.value === "scissors") {
+      player.src = "https://i.insider.com/5fc12c8850e71a0011557ae9?width=700";
+    }
+    if (e.target.value === "paper") {
+      player.src =
+        "https://ca-times.brightspotcdn.com/dims4/default/51bf23e/2147483647/strip/true/crop/750x394+0+14/resize/1200x630!/quality/90/?url=https%3A%2F%2Fwww.trbimg.com%2Fimg-5436b8c6%2Fturbine%2Fzap-the-office-michaels-worst-moments-pics-012";
+    }
+  }
+
+  // addPlayerNames() {
+  //   console.log(this.player1Name);
+  //   this.player1Name.textContent = `<h2>${this.game.player1.name}</h2>`;
+  //   this.player2Name.textContent = `<h2>${this.game.player2.name}</h2>`;
+  // }
+}
+
 class Player {
   constructor(name) {
     this.name = name;
@@ -20,12 +62,6 @@ class Player {
 
   resetPoints() {
     this.points = 0;
-  }
-}
-
-class gameVisuals {
-  constructor(game) {
-    this.game = game;
   }
 }
 
@@ -91,7 +127,8 @@ class Game {
   }
 }
 
-const game = new Game(new Player("John"), new Player("Kalynn"));
+const game = new Game(new Player(p1Name), new Player(p2Name));
+const gameUI = new gameVisuals(game);
 
 // playButton.addEventListener("click", (e) => {
 //   game.playRound();
